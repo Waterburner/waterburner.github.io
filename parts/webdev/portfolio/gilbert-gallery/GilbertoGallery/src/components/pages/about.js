80806daa1795864c03d477900a4c26057ade34f2
Waterburner TroubleMaker <Waterburner@users.noreply.github.com>
2025-01-18 20:12:29 -0600

    Delete parts/webdev/portfolio/gilbert-gallery/GilbertoGallery directory
    Seems like it's preventing from proper deployment.

--- parts/webdev/portfolio/gilbert-gallery/GilbertoGallery/src/components/pages/about.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from "react";
-
-export default function () {
-    return (
-        <div>
-            <h2>About page</h2>
-        </div>
-    );
-}
--- parts/webdev/portfolio/gilbert-gallery/GilbertoGallery/src/components/pages/contact.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from "react";
-
-export default function () {
-    return (
-        <div>
-            <h2>Contact page</h2>
-        </div>
-    );
-}
